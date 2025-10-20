c9768f808912a097a83ffb8453eadb7062244c59
Swapna Lekkala <swapna942@meta.com>
2025-10-20 14:37:00 -0700

    Add copyright headers to root config files
    - Added Meta Platforms copyright headers to jest.config.ts and .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,9 @@
+// Copyright (c) Meta Platforms, Inc. and affiliates.
+// All rights reserved.
+//
+// This source code is licensed under the terms described in the LICENSE file in
+// the root directory of this source tree.
+
 module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'unused-imports', 'prettier'],
