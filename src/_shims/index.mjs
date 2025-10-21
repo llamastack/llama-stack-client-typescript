bedd9c6f2c462c4af2b53dca1e4711f28b2b7bd9
slekkala1 <swapna.lekkala@gmail.com>
2025-10-21 16:03:41 -0700

    Update copyright and disclaimer comments in index.mjs

--- src/_shims/index.mjs
+++ src/_shims/index.mjs
@@ -1,4 +1,11 @@
 /**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/**
  * Disclaimer: modules in _shims aren't intended to be imported by SDK users.
  */
 import * as shims from './registry.mjs';
