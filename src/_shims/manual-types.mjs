9a861009d1d041028900d771b6af691d843cdca3
slekkala1 <swapna.lekkala@gmail.com>
2025-10-21 16:04:06 -0700

    Update copyright notice in manual-types.mjs

--- src/_shims/manual-types.mjs
+++ src/_shims/manual-types.mjs
@@ -1,3 +1,10 @@
 /**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/**
  * Disclaimer: modules in _shims aren't intended to be imported by SDK users.
  */
